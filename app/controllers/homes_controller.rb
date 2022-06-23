5a8401fde793b4394d98d30850d56a3ef92bc612
R1013-T <82691952+R1013-T@users.noreply.github.com>
2022-06-23 15:15:46 +0900

    first

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def main
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'main', to:"homes#main"
+  resource :user, only: [:new, :create, :show]
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  get 'logout', to: "homes#top"
+  delete 'logout', to: "homes#top"
+end
