ff5c4f13bce995d1a2642f9fc86ded83ac278af9
R1013-T <82691952+R1013-T@users.noreply.github.com>
2022-07-17 14:55:31 +0900

    audio file public?

--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -120,9 +120,9 @@ new Vue({
 
     
     
-    // correct_audio: new Audio('/assets/correct.mp3'),
-    // true_audio: new Audio('/assets/true.mp3'),
-    // false_audio: new Audio('/assets/false.mp3'),
+    correct_audio: new Audio('audios/correct.mp3'),
+    true_audio: new Audio('audios/true.mp3'),
+    false_audio: new Audio('audios/false.mp3'),
   },
   computed: {
     styleObject: function () {
